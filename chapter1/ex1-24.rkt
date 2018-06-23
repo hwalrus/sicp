3adb3300ebd5ba99c0ce817405da7de7965ac993
hwalrus <39668365+hwalrus@users.noreply.github.com>
2018-06-23 17:04:05 +0800

    minor update

--- chapter1/ex1-24.rkt
+++ chapter1/ex1-24.rkt
@@ -14,6 +14,7 @@
          (remainder (* base (expmod base (- exp 1) m))
                     m))))
 
+; note use of random-integer that's required for very large number.s
 (define (fermat-test n)
   (define (try-it a)
     (= (expmod a n n) a))
@@ -30,7 +31,7 @@
   (start-prime-test n (current-inexact-milliseconds)))
 
 (define (start-prime-test n start-time)
-  (cond ((fast-prime? n 3)
+  (cond ((fast-prime? n 100)
          (report-prime (- (current-inexact-milliseconds) start-time))
          #t)
         (else #f)))     
@@ -43,7 +44,7 @@
   (newline)
   (display "starting from ")
   (display greater-than)
-  (search-for greater-than 100))
+  (search-for greater-than 3))
 
 (define (search-for start primes-remaining)
   (cond ((> primes-remaining 0)
@@ -67,12 +68,17 @@
 ;
 ; So we'd expect the Fermat test to be considerably faster.
 ;
-; Running the above prime tests start from 100 billion, still takes 0 milliseconds to
-; find the primes. If we bump up the number of guesses to 100, then it starts to take
-; half a millisecond to calculate.
+; Starting at 10,000 we see that with a perhaps arbitrarily large maximum of 100 gueses
+; per number, it takes between 0 and 1 millisecond to get the next prime.
 ;
-; Even if we start looking from a very large starting point, it doesn't ever seem to
-; take more than half a millisecond.
+; However, when we increase this to 100 billion, the range has only increased from 1 to
+; 3 milliseconds, still takes 0 milliseconds compared to between 15 and 27 milliseconds
+; for the improved smallest divisor method.
+;
+; Even if we start looking from a very large starting point, such as 100 trillion, the
+; time taken is still around 3 milliseconds.
+;
+; This is consistent with log n growth (twice as many digits is about twice as slow)
 
 (search-for-primes 1000000000000)
 (search-for-primes 10000000000000)
